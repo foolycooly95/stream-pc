2deca6131469c4fe2522796d52de8488b3e54507
foolycooly95 <jackwoodman95@gmail.com>
2025-11-18 18:55:43 +0000

    Update deploy.sh to use Cloudflare environment variables.

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,8 @@
 mkdir output
 touch output/meta-data
 cp autoinstall.yaml output/user-data
+LOWER_USERNAME=$(echo "$NAME" | tr '[:upper:]' '[:lower:]')
+PASSWORD_HASH=$(openssl passwd -6 "$PASSWORD")
+sed -i "s|\${PASSWORD_HASH}|$PASSWORD_HASH|g" output/user-data
+sed -i "s|\${REALNAME}|$NAME|g" output/user-data
+sed -i "s|\${USERNAME}|$LOWER_USERNAME|g" output/user-data
