9f698d315784d2d171b804d82103328c1fa56a30
foolycooly95 <jackwoodman95@gmail.com>
2025-11-18 18:59:53 +0000

    Remove all comments except for the first line and any blank lines

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@ PASSWORD_HASH=$(openssl passwd -6 "$PASSWORD")
 sed -i "s|\${PASSWORD_HASH}|$PASSWORD_HASH|g" output/user-data
 sed -i "s|\${REALNAME}|$NAME|g" output/user-data
 sed -i "s|\${USERNAME}|$LOWER_USERNAME|g" output/user-data
+
+# Remove all comments except for the first line and any blank lines
+sed -i '1!{/^#/d; /^$/d}' output/user-data
